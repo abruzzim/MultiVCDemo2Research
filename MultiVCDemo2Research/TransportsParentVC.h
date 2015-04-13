90c5a2fd686d7e2fb4ad1ecdc8b5cb7fd920d0a1
abruzzim <abruzzim@framework-solutions.com>
2015-04-13 16:01:36 -0400

    Add Transports Parent VC Subclass.

---
+++ MultiVCDemo2Research/TransportsParentVC.h
@@ -0,0 +1,13 @@
+//
+//  TransportsParentVC.h
+//  MultiVCDemo2Research
+//
+//  Created by dev1 on 4/13/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransportsParentVC : UIViewController
+
+@end
---
+++ MultiVCDemo2Research/TransportsParentVC.m
@@ -0,0 +1,37 @@
+//
+//  TransportsParentVC.m
+//  MultiVCDemo2Research
+//
+//  Created by dev1 on 4/13/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import "TransportsParentVC.h"
+
+@interface TransportsParentVC ()
+
+@end
+
+@implementation TransportsParentVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
