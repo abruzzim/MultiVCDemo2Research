e2a0448bbfa8aa054204772949fac9eeb3696dc8
abruzzim <abruzzim@framework-solutions.com>
2015-04-13 15:43:43 -0400

    Initial Commit

---
+++ MultiVCDemo2Research/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiVCDemo2Research
+//
+//  Created by dev1 on 4/13/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiVCDemo2Research/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultiVCDemo2Research
+//
+//  Created by dev1 on 4/13/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
