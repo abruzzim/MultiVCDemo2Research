492507a9a72bebc02ede1cf909e34e53ee0aa42a
abruzzim <abruzzim@framework-solutions.com>
2015-04-13 15:54:35 -0400

    Add Main Tab Bar Controller Subclass.

---
+++ MultiVCDemo2Research/MainTBC.h
@@ -0,0 +1,13 @@
+//
+//  MainTBC.h
+//  MultiVCDemo2Research
+//
+//  Created by dev1 on 4/13/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTBC : UITabBarController
+
+@end
---
+++ MultiVCDemo2Research/MainTBC.m
@@ -0,0 +1,39 @@
+//
+//  MainTBC.m
+//  MultiVCDemo2Research
+//
+//  Created by dev1 on 4/13/15.
+//  Copyright (c) 2015 dev1. All rights reserved.
+//
+
+#import "MainTBC.h"
+
+@interface MainTBC ()
+
+@end
+
+@implementation MainTBC
+
+- (void)viewDidLoad {
+    NSLog(@"%%MainTBC-I-TRACE, -viewDidLoad called.");
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    NSLog(@"%%MainTBC-I-TRACE, -didReceiveMemoryWarning called.");
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
